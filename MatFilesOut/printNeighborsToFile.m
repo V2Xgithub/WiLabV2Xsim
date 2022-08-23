d10ebc0b9b8b0bee1cc149de493320c6a1d1a323
吴卓霏_Felix <wzfhrb.cn@gmail.com>
2022-08-23 09:44:24 +0200

    debug output of neighbots

--- MatFilesOut/printNeighborsToFile.m
+++ MatFilesOut/printNeighborsToFile.m
@@ -9,10 +9,12 @@ fprintf(fileID,'%f\t',time);
 for k = 1:length(phyParams.Raw)
     if k==1
         distanceCheck = positionManagement.distanceReal(:,:)<phyParams.Raw(1);
+        avNeighborsTot = (sum(sum(distanceCheck))-size(distanceCheck,1))/Nvehicles;
     else
-        distanceCheck = (positionManagement.distanceReal(:,:)>=phyParams.Raw(k-1) & positionManagement.distanceReal(:,:)>phyParams.Raw(k));
+        distanceCheck = (positionManagement.distanceReal(:,:)>=phyParams.Raw(k-1) & positionManagement.distanceReal(:,:)<phyParams.Raw(k));
+        avNeighborsTot = sum(sum(distanceCheck))/Nvehicles;
     end
-    avNeighborsTot = sum(sum(distanceCheck))/Nvehicles;
+    
     fprintf(fileID,'%d\t%d\t%d',averageNeighborsRawLTE(k),averageNeighborsRaw11p(k),avNeighborsTot);
     if k<length(phyParams.Raw)
         fprintf(fileID,'\t');
